12fb71209901ab9149ee46afaa81a1e824021ae9
Mário Lucas <mariolucasdev@gmail.com>
2023-07-31 01:23:52 -0300

    aula-9: adding styles

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,12 @@
   </nav>
   <router-view/>
 </template>
+
+<style>
+@import url('https://pro.fontawesome.com/releases/v5.10.0/css/all.css');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
+</style>
+
+<style lang="sass">
+@import "./assets/styles/eti.scss"
+</style>
