8c7f40c47951845091e3014f5599c17e7475247b
Mário Lucas <mariolucasdev@gmail.com>
2023-07-30 21:22:31 -0300

    aula-2: mount struture to Todo component

--- src/views/Todos/Todos.vue
+++ src/views/Todos/Todos.vue
@@ -1,3 +1,35 @@
 <template>
     <h1> Lista de Tarefas </h1>
+
+    <ul>
+        <li v-for="todo in todos" :key="todo.id">
+            {{ todo.title }}
+        </li>
+    </ul>
+
+    <input type="text" v-model="name">
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+    name: 'Todos',
+    setup() {
+        const todos = [
+            { id: 1, title: 'Tarefa 1' },
+            { id: 2, title: 'Tarefa 2' },
+            { id: 3, title: 'Tarefa 3' },
+            { id: 4, title: 'Tarefa 4' },
+            { id: 5, title: 'Tarefa 5' },
+        ]
+
+        const name = ref('default value')
+
+        return {
+            todos,
+            name
+        }
+    }
+}
+</script>
